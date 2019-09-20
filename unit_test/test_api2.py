8b75b3b8c0a3d42ae054373141a9ea47abd7ebd2
Mingxun Wang <mwang87@gmail.com>
2019-09-20 14:50:17 -0700

    adding inchikey test

--- unit_test/test_api2.py
+++ unit_test/test_api2.py
@@ -1,10 +1,13 @@
 import requests
 
-def test_1():
-    print("MING")
+URL_BASE = "https://gnps-structure.ucsd.edu"
 
+def test_smiles_to_inchikey():
+    url = URL_BASE + "/inchikey?smiles=CCC(C)C1N(C)C(=O)C2CCCN2C(=O)C(OC(=O)C(Cc2ccccc2)N(C)C(=O)C(NC(=O)C(C)C(CCCCC)OC1=O)C(C)C)C(C)C"
+    r = requests.get(url)
+    r.raise_for_status()
 
-    assert(1 == 0)
+    assert(r.text == "NIFSOTSGUFBSPF-UHFFFAOYSA-N")
 
 def test_2():
     return 0
